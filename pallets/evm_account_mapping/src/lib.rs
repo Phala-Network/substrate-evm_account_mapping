e4779faaa498e80b604852fbb7b2061c6e7fd86f
Jun Jiang <jasl9187@hotmail.com>
2024-01-08 07:54:18 +0800

    cargo clippy

--- pallets/evm_account_mapping/src/lib.rs
+++ pallets/evm_account_mapping/src/lib.rs
@@ -277,7 +277,7 @@ pub mod pallet {
 			// Check the signature and get the public key
 			let call_data = <T as Config>::RuntimeCall::encode(call);
 			let message_hash =
-				Self::eip712_message_hash(&who, &call_data, nonce, tip, pre_signed_cheque);
+				Self::eip712_message_hash(who, &call_data, nonce, tip, pre_signed_cheque);
 
 			let Ok(recovered_public_key) =
 				(match <T as Config>::AddressConverter::SECP256K1_PUBLIC_KEY_FORM {
@@ -311,7 +311,7 @@ pub mod pallet {
 			// Skip frame_system::CheckEra<Runtime>
 
 			// frame_system::CheckNonce<Runtime>
-			let account_nonce = AccountNonce::<T>::get(&who);
+			let account_nonce = AccountNonce::<T>::get(who);
 			if nonce < &account_nonce {
 				return Err(InvalidTransaction::Stale.into());
 			}
@@ -391,7 +391,7 @@ pub mod pallet {
 				};
 
 				let usable_balance_for_fees = T::Currency::reducible_balance(
-					&signer,
+					signer,
 					Preservation::Preserve,
 					Fortitude::Polite,
 				)
@@ -407,7 +407,7 @@ pub mod pallet {
 				}
 			} else {
 				let usable_balance_for_fees =
-					T::Currency::reducible_balance(&who, Preservation::Preserve, Fortitude::Polite)
+					T::Currency::reducible_balance(who, Preservation::Preserve, Fortitude::Polite)
 						.saturated_into::<u128>();
 				if est_fee.saturating_add(service_fee) > usable_balance_for_fees {
 					return Err(InvalidTransaction::Payment.into());
